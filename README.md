0c901bb932dbfd7c438db4476016142a4cb3ef54
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-04-25 13:03:31 +0200

    v 1.00

---
+++ README.md
@@ -0,0 +1,11 @@
+Dragon Slayer - a simple browser game.
+
+The challenge is to kill the dragon.
+
+The player can perform an attack, a special attack, and heal.
+
+Special attack and healing are only active in some rounds.
+
+The dragon always strikes back.
+
+You have to choose the right tactics and kill the dragon to save your kingdom!
---
+++ app.js
@@ -0,0 +1,102 @@
+function getRandomValue(max, min) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
+      logMessages: [],
+    };
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "Draw";
+      } else if (value <= 0) {
+        this.winner = "Monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "Draw";
+      } else if (value <= 0) {
+        this.winner = "Player";
+      }
+    },
+  },
+  computed: {
+    monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.playerHealth + "%" };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % getRandomValue(6, 2) !== 0;
+    },
+    mayUseHeal() {
+      return this.currentRound % getRandomValue(6, 2) !== 0;
+    },
+  },
+  methods: {
+    startGame() {
+      this.logMessages = [];
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 1;
+      this.winner = null;
+    },
+    attackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(12, 5);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+      this.addLogMessage("player", "attack", attackValue);
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(15, 8);
+      this.playerHealth -= attackValue;
+      this.addLogMessage("monster", "attack", attackValue);
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(20, 10);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+      this.addLogMessage("player", "special-attack", attackValue);
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(20, 12);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+      this.addLogMessage("player", "heal", healValue);
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
+    },
+    surrender() {
+      this.winner = "Monster";
+    },
+  },
+});
+
+app.mount("#game");
